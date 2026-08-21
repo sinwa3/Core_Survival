ccfc386a386a7a15f77a640d10c54210319abec2
sinwa3 <sinwa3275@gmail.com>
2026-08-21 18:25:03 +0900

    feat: Create temp title UI

---
+++ Assets/Scripts/Runtime/Title.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStartGame()
+    {
+        Debug.Log("게임 시작 버튼 눌림");
+    }
+
+    public void OnClickOption()
+    {
+        Debug.Log("옵션 버튼 눌림");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

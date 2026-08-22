1dc0455ce283f125611f4c329c27bbd3884a4da8
sinwa3 <sinwa3275@gmail.com>
2026-08-22 12:09:00 +0900

    refactor: move scene management logic to SceneFlowManager
    remove GetScenes method in SceneCatalog.cs
    add LoadScene, SetCurrentSceneIndex method

--- Assets/Scripts/Runtime/Scene/SceneflowManager.cs
+++ Assets/Scripts/Runtime/Scene/SceneflowManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneflowManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class SceneflowManager : MonoBehaviour
 
     #region 내부 변수
     public static SceneflowManager instance;
+    private int _currentSceneIndex;
     #endregion
 
     private void Awake()
@@ -31,7 +33,7 @@ public class SceneflowManager : MonoBehaviour
 
         if (instance != null && instance != this)
         {
-            Debug.LogWarning("인스턴스 중복 / 새 인스턴스 생성 불가");
+            Debug.LogWarning("씬 인스턴스 중복 / 새 인스턴스 생성 불가");
             Destroy(this.gameObject);
             return;
         }
@@ -41,15 +43,58 @@ public class SceneflowManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
 
         _catalog.SettingDictionary();
+
+        SetCurrentSceneIndex();
     }
 
     void Start()
     {
-        _transition.InitFadeGroup();
+        if (_transition != null)
+        {
+            _transition.InitFadeGroup();
+        }
     }
 
     void Update()
     {
-        
+
+    }
+
+    private void SetCurrentSceneIndex()
+    {
+        IReadOnlyList<SceneInfo> scenes = _catalog.Scenes;
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        for (int i = 0; i < scenes.Count; i++)
+        {
+            if (sceneName == scenes[i].sceneName)
+            {
+                _currentSceneIndex = i;
+
+                return;
+            }
+        }
+
+        Debug.LogWarning("현재 씬 인덱스 찾지 못함");
+        _currentSceneIndex = 0;
+    }
+
+    public void LoadScene(ESceneID id)
+    {
+        if (!_catalog.TryGetSceneName(id, out string name))
+        {
+            Debug.LogWarning("잘못된 ID / 씬 전환 불가");
+
+            return;
+        }
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Debug.LogWarning("씬 이름 없음 / 씬 전환 불가");
+
+            return;
+        }
+
+        SceneManager.LoadScene(name);
     }
 }

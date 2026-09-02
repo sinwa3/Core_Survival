1b8823ad4045c21f1b0d376ff2f4b7c5f8da212d
sinwa3 <sinwa3275@gmail.com>
2026-09-02 23:05:24 +0900

    feat: Add background music, UI click and level up sounds

---
+++ Assets/Scripts/Runtime/UI/BGMPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour
+{
+    #region 인스펙터
+    [SerializeField] private GameManager _gameManager;
+    #endregion
+
+    #region 내부 변수
+    private AudioSource _audioSource;
+    #endregion
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_audioSource == null)
+        {
+            Debug.LogWarning("AudioSource 컴포넌트 null (BGMPlayer) / 확인 요망");
+            enabled = false;
+
+            return;
+        }
+
+        _audioSource.ignoreListenerPause = true;
+    }
+
+    private void OnEnable()
+    {
+        if (_gameManager != null)
+        {
+            _gameManager.OnStateChanged += StateChanged;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(_gameManager != null)
+        {
+            _gameManager.OnStateChanged -= StateChanged;
+        }
+    }
+
+    private void StateChanged(EGameState state)
+    {
+        switch (state)
+        {
+            case EGameState.Paused:
+                _audioSource.Pause();
+                break;
+            case EGameState.GameOver:
+                _audioSource.Stop();
+                break;
+            case EGameState.Playing:
+                _audioSource.UnPause();
+                break;
+            case EGameState.LevelUp:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/UI/UISound.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISound : MonoBehaviour
+{
+    #region 인스펙터
+    [Header("사운드 클립")]
+    [SerializeField] private AudioClip _clickClip;
+    [SerializeField] private AudioClip _levelUpClip;
+
+    [SerializeField] private PlayerLevel _levelUpEvent;
+    #endregion
+
+    #region 내부 변수
+    private AudioSource _audioSource;
+    #endregion
+
+    private void OnEnable()
+    {
+        if (_levelUpEvent != null)
+        {
+            _levelUpEvent.OnLevelUp += LevelUpSoundPlay;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_levelUpEvent != null)
+        {
+            _levelUpEvent.OnLevelUp -= LevelUpSoundPlay;
+        }
+    }
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_audioSource == null)
+        {
+            Debug.LogWarning("AudioSource 컴포넌트 null (UISound) / 확인 요망");
+            enabled = false;
+
+            return;
+        }
+
+        _audioSource.ignoreListenerPause = true;
+
+
+    }
+
+    void Start()
+    {
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            Button button = buttons[i];
+            button.onClick.AddListener(ClickSoundPlay);
+        }
+    }
+
+    private void ClickSoundPlay()
+    {
+        if(_clickClip == null)
+        {
+            return;
+        }
+
+        _audioSource.PlayOneShot(_clickClip);
+    }
+
+    private void LevelUpSoundPlay()
+    {
+        if(_levelUpClip == null)
+        {
+            return;
+        }
+
+        _audioSource.PlayOneShot(_levelUpClip);
+    }
+}

b1013e00e09acec3f16099dcc3b16b118122d6ab
sinwa3 <sinwa3275@gmail.com>
2026-09-07 11:40:39 +0900

    feat: Reskin UI with nine-sliced borders and button hover feedback

---
+++ Assets/Scripts/Runtime/UI/ButtonHover.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    #region 인스펙터
+    [Header("텍스트")]
+    [SerializeField] private TMP_Text _text;
+
+    [Header("색")]
+    [SerializeField] private Color _hoverColor = Color.white;
+
+    #endregion
+
+    #region 내부 변수
+    private Color _baseColor;
+    #endregion
+
+    private void Awake()
+    {
+        if (_text == null)
+        {
+            _text = GetComponentInChildren<TMP_Text>();
+        }
+
+        if (_text == null)
+        {
+            Debug.LogWarning("텍스트 null (ButtonHover) / 인스펙터 확인");
+            enabled = false;
+
+            return;
+        }
+
+        _baseColor = _text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _text.color = _hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _text.color = _baseColor;
+    }
+}
